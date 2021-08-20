cfcda256e8a980f1c03f565925d663002ff9c0c6
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-08-19 20:01:27 -0400

    header

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
     // note you must disable the base rule as it can report incorrect errors
     'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
   },
 };
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+const Header: FC = () => (
+  <nav>
+    <div className="black-bar general" />
+    <div className="title-container">
+      <h1>Rodrigo Ibaceta</h1>
+      <h3>Full-stack web developer</h3>
+    </div>
+  </nav>
+);
+
+export default Header;
